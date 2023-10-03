152429cabb0cfb8790ce1106ccc0ea138e2f0274
Cláudio Peruna <94857742+cldaniel101@users.noreply.github.com>
2023-10-02 23:01:02 -0300

    Criando Interface

---
+++ cores.py
@@ -0,0 +1,50 @@
+COR_GRADE = "#a39489"
+COR_CELULA_VAZIA = "#c2b3a9"
+FONTE_ROTULO_PONTUACAO = ("Verdana", 20)
+FONTE_PONTUACAO = ("Helvetica", 32, "bold")
+FONTE_FIM_DE_JOGO = ("Helvetica", 48, "bold")
+COR_FONTE_FIM_DE_JOGO = "#ffffff"
+FUNDO_VENCEDOR = "#ffcc00"
+FUNDO_PERDEDOR = "#a39489"
+
+CORES_CELULA = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "#fad74d"
+}
+
+CORES_NUMERO_CELULA = {
+    2: "#695c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+FONTE_NUMERO_CELULA = {
+    2: ("Helvetica", 55, "bold"),
+    4: ("Helvetica", 55, "bold"),
+    8: ("Helvetica", 55, "bold"),
+    16: ("Helvetica", 50, "bold"),
+    32: ("Helvetica", 50, "bold"),
+    64: ("Helvetica", 50, "bold"),
+    128: ("Helvetica", 45, "bold"),
+    256: ("Helvetica", 45, "bold"),
+    512: ("Helvetica", 45, "bold"),
+    1024: ("Helvetica", 40, "bold"),
+    2048: ("Helvetica", 40, "bold")
+}
--- main.py
+++ main.py
@@ -1,208 +1,226 @@
+import tkinter as tk
 import random
-import os
-if os.name == 'nt':
-    import msvcrt
+import cores
 
 score = 0
 
-def start_game():
-    global matriz
-    matriz = [[0] * 4 for _ in range(4)]
+class Jogo(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)  # construtor do Tkinter para inicializar a janela
+        self.grid()  # organiza os widgets dentro da janela
+        self.master.title('2048')  # título da janela
 
-    row = random.randint(0, 3)
-    col = random.randint(0, 3)
-    matriz[row][col] = 2
+        # Aqui, está sendo criado um quadro principal dentro da janela e colocando na grade principal
+        self.main_grid = tk.Frame(
+            self, bg=cores.COR_GRADE, bd=3, width=400, height=400) 
+        self.main_grid.grid(pady=(80, 0)) 
 
-    while(matriz[row][col] != 0):
+        # cria a interface gráfica e inicia o jogo
+        self.criar_Interface()
+        self.start_game()
+
+        # controlar o movimento das peças 
+        self.master.bind("<Left>", self.esquerda)
+        self.master.bind("<Right>", self.direita)
+        self.master.bind("<Up>", self.cima)
+        self.master.bind("<Down>", self.baixo)
+
+        # Mantém o jogo aberto até que a janela seja fechada
+        self.mainloop()
+
+
+    def criar_Interface(self):
+        self.celulas = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                quadrado_celula = tk.Frame(
+                    self.main_grid,
+                    bg=cores.COR_CELULA_VAZIA,
+                    width=100,
+                    height=100)
+                quadrado_celula.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg=cores.COR_CELULA_VAZIA)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": quadrado_celula, "number": cell_number}
+                row.append(cell_data)
+            self.celulas.append(row)
+
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=40, anchor="center")
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=cores.FONTE_ROTULO_PONTUACAO).grid(
+            row=0)
+        self.score_label = tk.Label(score_frame, text="0", font=cores.FONTE_PONTUACAO)
+        self.score_label.grid(row=1)
+
+    def start_game(self):
+        self.matriz = [[0] * 4 for _ in range(4)]
+
         row = random.randint(0, 3)
         col = random.randint(0, 3)
-    matriz[row][col] = 2
+        self.matriz[row][col] = 2
 
-    print("""
-    BOTÕES: 
-    ↓ ← → ↑ - Teclas de Direção
-    q - Finaliza o Jogo
-    """)
+        self.celulas[row][col]["frame"].configure(bg=cores.CORES_CELULA[2])
+        self.celulas[row][col]["number"].configure(
+            bg=cores.CORES_CELULA[2],
+            fg=cores.CORES_NUMERO_CELULA[2],
+            font=cores.FONTE_NUMERO_CELULA[2],
+            text="2")
 
-    mostra_a_matriz()
-
-def empilhar():
-    global matriz
-    nova_matriz = [[0] * 4 for _ in range(4)]
-    for row in range(4):
-        posicao_preenchida = 0
-        for col in range(4):
-            if matriz[row][col] != 0:
-                nova_matriz[row][posicao_preenchida] = matriz[row][col]
-                posicao_preenchida += 1
-    matriz = nova_matriz
-
-def combinar():
-    global matriz
-    global score
-    for row in range(4):
-        for col in range(3):
-            if matriz[row][col] != 0 and matriz[row][col] == matriz[row][col + 1]:
-                matriz[row][col] *= 2
-                matriz[row][col + 1] = 0
-                score += matriz[row][col]
-
-def reverter():
-    global matriz
-    nova_matriz = []
-    for row in range(4):
-        nova_matriz.append([])
-        for col in range(4):
-            nova_matriz[row].append(matriz[row][3 - col])
-    matriz = nova_matriz
-
-def transpor():
-    global matriz
-    nova_matriz = [[0] * 4 for _ in range(4)]
-    for row in range(4):
-        for col in range(4):
-            nova_matriz[row][col] = matriz[col][row]
-    matriz = nova_matriz
-
-def add_nova_peca():
-    global matriz
-    if any(0 in row for row in matriz):
-        row = random.randint(0, 3)
-        col = random.randint(0, 3)
-        while(matriz[row][col] != 0):
+        while(self.matriz[row][col] != 0):
             row = random.randint(0, 3)
             col = random.randint(0, 3)
-        if random.random() < 0.9:
-            matriz[row][col] = 2
-        else:
-            matriz[row][col] = 4
+        self.matriz[row][col] = 2
 
-def mostra_a_matriz():
-    global matriz
-    print()
-    print(f"Score: {score}")
-    for row in range(4):
-        print(matriz[row])
+        self.celulas[row][col]["frame"].configure(bg=cores.CORES_CELULA[2])
+        self.celulas[row][col]["number"].configure(
+            bg=cores.CORES_CELULA[2],
+            fg=cores.CORES_NUMERO_CELULA[2],
+            font=cores.FONTE_NUMERO_CELULA[2],
+            text="2")
 
-def horizontal_move_exists():
-    global matriz
-    for row in range(4):
-        for col in range(3):
-            if matriz[row][col] == matriz[row][col + 1]:
-                return True
-    return False
+        self.score = 0
 
-def vertical_move_exists():
-    global matriz
-    for row in range(3):
-        for col in range(4):
-            if matriz[row][col] == matriz[row + 1][col]:
-                return True
-    return False
 
-def fim_de_jogo():
-    global matriz
-    global parar
-    if any(2048 in row for row in matriz):
-        print("VITÓRIA !!")
-        parar = False
-    elif not any(0 in row for row in matriz) and not horizontal_move_exists() and not vertical_move_exists():
-        print("VOCÊ PERDEU !!")
-        parar = False
+    def empilhar(self):
+        nova_matriz = [[0] * 4 for _ in range(4)]
+        for row in range(4):
+            posicao_preenchida = 0
+            for col in range(4):
+                if self.matriz[row][col] != 0:
+                    nova_matriz[row][posicao_preenchida] = self.matriz[row][col]
+                    posicao_preenchida += 1
+        self.matriz = nova_matriz
 
-def limpar_terminal():
-    # Verifica se o sistema operacional é Windows
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        # Limpa o terminal em sistemas não-Windows (Linux, macOS, etc.)
-        os.system('clear')
+    def combinar(self):
+        for row in range(4):
+            for col in range(3):
+                if self.matriz[row][col] != 0 and self.matriz[row][col] == self.matriz[row][col + 1]:
+                    self.matriz[row][col] *= 2
+                    self.matriz[row][col + 1] = 0
+                    self.score += self.matriz[row][col]
 
-def esquerda():
-    limpar_terminal()
-    empilhar()
-    combinar()
-    empilhar()
-    add_nova_peca()
-    mostra_a_matriz()
-    fim_de_jogo()
+    def reverter(self):
+        nova_matriz = []
+        for row in range(4):
+            nova_matriz.append([])
+            for col in range(4):
+                nova_matriz[row].append(self.matriz[row][3 - col])
+        self.matriz = nova_matriz
 
-def direita():
-    limpar_terminal()
-    reverter()
-    empilhar()
-    combinar()
-    empilhar()
-    reverter()
-    add_nova_peca()
-    mostra_a_matriz()
-    fim_de_jogo()
+    def transpor(self):
+        nova_matriz = [[0] * 4 for _ in range(4)]
+        for row in range(4):
+            for col in range(4):
+                nova_matriz[row][col] = self.matriz[col][row]
+        self.matriz = nova_matriz
 
-def cima():
-    limpar_terminal()
-    transpor()
-    empilhar()
-    combinar()
-    empilhar()
-    transpor()
-    add_nova_peca()
-    mostra_a_matriz()
-    fim_de_jogo()
-
-def baixo():
-    limpar_terminal()
-    transpor()
-    reverter()
-    empilhar()
-    combinar()
-    empilhar()
-    reverter()
-    transpor()
-    add_nova_peca()
-    mostra_a_matriz()
-    fim_de_jogo()
-
-# Comandos em sistemas não-Windows (Linux, macOS, etc.)
-def linux_mac():
-    parar = True
-    while parar:
-        print("\n[a] - Esquerda; [d] - Direita; [s] - Baixo; [w] - Cima \n[sair] - Finalizar Jogo")
-        tecla = input(">>> ")
-        if tecla == 's':
-            baixo()
-        elif tecla == 'w':
-            cima()
-        elif tecla == 'a':
-            esquerda()
-        elif tecla == 'd':
-            direita()
-        elif tecla == "sair":
-            parar = False
-        else:
-            print("\033[1;30;31mTecla Inválida\033[0m")
-
-# Comandos para Windows
-def windows():
-    key = msvcrt.getch()
-    while parar:
-        if key == b'q': 
-            parar = False  # Sair do loop se 'q' for pressionado
-        elif key == b'\xe0':
-            key = msvcrt.getch()
-            if key == b'H':
-                cima()
-            elif key == b'P':
-                baixo()
-            elif key == b'M':
-                direita()
-            elif key == b'K':
-                esquerda()
+    def add_nova_peca(self):
+        if any(0 in row for row in self.matriz):
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+            while(self.matriz[row][col] != 0):
+                row = random.randint(0, 3)
+                col = random.randint(0, 3)
+            if random.random() < 0.9:
+                self.matriz[row][col] = 2
             else:
-                print("\033[1;30;31mTecla Inválida\033[0m")
+                self.matriz[row][col] = 4
 
-start_game()
+    def atualizar_Interface(self):
+        for i in range(4):
+            for j in range(4):
+                cell_value = self.matriz[i][j]
+                if cell_value == 0:
+                    self.celulas[i][j]["frame"].configure(bg=cores.COR_CELULA_VAZIA)
+                    self.celulas[i][j]["number"].configure(
+                        bg=cores.COR_CELULA_VAZIA, text="")
+                else:
+                    self.celulas[i][j]["frame"].configure(
+                        bg=cores.CORES_CELULA[cell_value])
+                    self.celulas[i][j]["number"].configure(
+                        bg=cores.CORES_CELULA[cell_value],
+                        fg=cores.CORES_NUMERO_CELULA[cell_value],
+                        font=cores.FONTE_NUMERO_CELULA[cell_value],
+                        text=str(cell_value))
+        self.score_label.configure(text=self.score)
+        self.update_idletasks()
 
-if os.name == 'nt':
-    windows()
-else:
-    linux_mac()
+    def esquerda(self, event):
+        self.empilhar()
+        self.combinar()
+        self.empilhar()
+        self.add_nova_peca()
+        self.atualizar_Interface()
+        self.fim_de_jogo()
+
+    def direita(self, event):
+        self.reverter()
+        self.empilhar()
+        self.combinar()
+        self.empilhar()
+        self.reverter()
+        self.add_nova_peca()
+        self.atualizar_Interface()
+        self.fim_de_jogo()
+
+    def cima(self, event):
+        self.transpor()
+        self.empilhar()
+        self.combinar()
+        self.empilhar()
+        self.transpor()
+        self.add_nova_peca()
+        self.atualizar_Interface()
+        self.fim_de_jogo()
+
+    def baixo(self, event):
+        self.transpor()
+        self.reverter()
+        self.empilhar()
+        self.combinar()
+        self.empilhar()
+        self.reverter()
+        self.transpor()
+        self.add_nova_peca()
+        self.atualizar_Interface()
+        self.fim_de_jogo()
+
+    def horizontal_move_exists(self):
+        for row in range(4):
+            for col in range(3):
+                if self.matriz[row][col] == self.matriz[row][col + 1]:
+                    return True
+        return False
+
+    def vertical_move_exists(self):
+        for row in range(3):
+            for col in range(4):
+                if self.matriz[row][col] == self.matriz[row + 1][col]:
+                    return True
+        return False
+
+    def fim_de_jogo(self):
+        if any(2048 in row for row in self.matriz):
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            tk.Label(
+                game_over_frame,
+                text="VITÓRIA!",
+                bg=cores.FUNDO_VENCEDOR,
+                fg=cores.COR_FONTE_FIM_DE_JOGO,
+                font=cores.FONTE_FIM_DE_JOGO).pack()
+        elif not any(0 in row for row in self.matriz) and not self.horizontal_move_exists() and not self.vertical_move_exists():
+            game_over_frame = tk.Frame(self.main_grid, borderwidth=2)
+            game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
+            tk.Label(
+                game_over_frame,
+                text="DERROTA!",
+                bg=cores.FUNDO_PERDEDOR,
+                fg=cores.COR_FONTE_FIM_DE_JOGO,
+                font=cores.FONTE_FIM_DE_JOGO).pack()
+            
+Jogo()
